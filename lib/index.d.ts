d661d4b583dc543f70d66e8f777bdb7c5153178d
Anantachai Saothong (Manta) <anantachai.s@taskworld.com>
2023-03-02 17:09:31 +0700

    chore: no output bundles

--- lib/index.d.ts
+++
@@ -1,7 +0,0 @@
-export declare const allowedTypes: string[];
-export declare function checkConventionalMessage(message: string, { debug }: Pick<Console, 'debug'>): {
-    type: string | undefined;
-    breaking: boolean;
-    subject: string;
-    errors: string[];
-};
