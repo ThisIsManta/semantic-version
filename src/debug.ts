2c48db0c5b07dd620c1b5beb16c44ed621eddede
Anantachai Saothong (Manta) <anantachai.s@taskworld.com>
2024-01-07 15:36:27 +0700

    chore: support debug logging on GitHub Actions

--- src/debug.ts
+++ src/debug.ts
@@ -1,5 +1,5 @@
 export function debug(...args: Array<any>) {
-	if (process.env.DEBUG) {
+	if (process.env.DEBUG || process.env.RUNNER_DEBUG /* For GitHub Actions */) {
 		console.log(...args)
 	}
 }
